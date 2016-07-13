d7f5b9c993eec6a6610b1ddd6b1b91b229b23f14
ajaiswal <anuragjaiswal.07@gmail.com>
2016-07-13 16:52:33 +0530

    removed commented code

--- 05/5.hs
+++ 05/5.hs
@@ -7,24 +7,15 @@ eval :: ExprT -> Integer
 eval (Lit a) = a
 eval (Add l r) = (eval l) + (eval r)
 eval (Mul l r) = (eval l) * (eval r)
+
 --(Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
 
 --Ex2
 evalStr :: String -> Maybe Integer
---evalStr s 
---	| r == Nothing = Nothing
---	| r == Just x = eval x
---	where r = parseExp Lit Add Mul s
-
 evalStr s = let r = parseExp Lit Add Mul s in
 	case r of Nothing -> Nothing
 		  Just n -> Just (eval n)
-
---evalStr s = case (f s) of
---         Just n -> n
---         Nothing -> Nothing
---		where f = parseExp Lit Add Mul
-
+		  
 --evalStr "2+3*4"
 --Just 14
 -- evalStr "2+3*"
