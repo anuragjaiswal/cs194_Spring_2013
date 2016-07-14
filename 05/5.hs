955720e9398cc25adab2983709640a62fcb0731c
ajaiswal <anuragjaiswal.07@gmail.com>
2016-07-14 12:55:28 +0530

    Completed Ex 5

--- 05/5.hs
+++ 05/5.hs
@@ -25,5 +25,39 @@ evalStr s = let r = parseExp Lit Add Mul s in
 
 
 -- Ex3
+class Expr a where
+	lit :: Integer -> a
+	add :: a -> a -> a
+	mul :: a -> a -> a
+	
+--instance Expr ExprT where
+--    lit x = Lit x
+--	add a b = Add a b
+--	mul a b = Mul a b
+    
 
+--Ex4
+
+instance Expr Integer where
+	lit x = x
+	add x y = x + y
+	mul x y = x * y
+	
+instance Expr Bool where
+	lit x = if (x <= 0) then False else True
+	add x y =  x || y
+	mul x y =  x && y
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+	lit x = MinMax x
+	add (MinMax x) (MinMax y) = MinMax (max x y)
+	mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+	lit x = Mod7 (x `mod` 7)
+	add (Mod7 x) (Mod7 y) = Mod7 (x+y)
+	mul (Mod7 x) (Mod7 y) = Mod7 (x*y)
 
