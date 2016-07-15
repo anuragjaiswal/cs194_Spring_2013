b54d214008d863bf22392837bd71a25246c4301b
ajaiswal <anuragjaiswal.07@gmail.com>
2016-07-15 11:12:03 +0530

    Partial Ex6 of Homework 5

--- 05/5.hs
+++ 05/5.hs
@@ -1,18 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Ex05 where
+import qualified Data.Map as M
 import ExprT
 import Parser
 
 --Ex 1
 eval :: ExprT -> Integer
-eval (Lit a) = a
-eval (Add l r) = (eval l) + (eval r)
-eval (Mul l r) = (eval l) * (eval r)
+eval (ExprT.Lit a) = a
+eval (ExprT.Add l r) = (eval l) + (eval r)
+eval (ExprT.Mul l r) = (eval l) * (eval r)
 
 --(Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
 
 --Ex2
 evalStr :: String -> Maybe Integer
-evalStr s = let r = parseExp Lit Add Mul s in
+evalStr s = let r = parseExp ExprT.Lit ExprT.Add ExprT.Mul s in
 	case r of Nothing -> Nothing
 		  Just n -> Just (eval n)
 		  
@@ -61,3 +63,39 @@ instance Expr Mod7 where
 	add (Mod7 x) (Mod7 y) = Mod7 (x+y)
 	mul (Mod7 x) (Mod7 y) = Mod7 (x*y)
 
+-- Ex6
+class HasVars a where
+	var :: String -> a
+
+data VarExprT = Var String 
+	   | Lit Integer
+	   | Add VarExprT VarExprT
+	   | Mul VarExprT VarExprT
+	deriving (Show, Eq)
+	
+instance Expr VarExprT where
+	lit x = Ex05.Lit x
+	add x y = Ex05.Add x y
+	mul x y = Ex05.Mul x y
+
+instance HasVars VarExprT where
+	var str = Var str     
+ 
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+	var s = (\m -> M.lookup s m)
+	
+instance Expr  (M.Map String Integer -> Maybe Integer) where
+	lit x = (\_ -> Just x)
+
+--ithVars :: [(String, Integer)]
+--			-> (M.Map String Integer -> Maybe Integer)
+--			-> Maybe Integer
+--			
+--
+--instance HasVars VarExprT where
+--	var x = 
+--	
+--instance Expr VarExprT where
+--	lit x = x
+--	add x y = x + y
+--	mul x y = x * y
