53fb349eacc7a726dec7f25e5260690305f60532
ajaiswal <anuragjaiswal.07@gmail.com>
2016-07-18 17:43:35 +0530

    Ex 1-3 of Homework 6

--- 06/6.hs
+++ 06/6.hs
@@ -16,3 +16,19 @@ fibs1 = map fib [1,2..]
 --Ex02
 fibs2 :: [Integer]
 fibs2 = take 20(1:1:zipWith(+) fibs2 (tail fibs2))
+
+--Ex03
+data Stream a = SCons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (SCons x xs) = x:(streamToList xs)
+
+instance Show a => Show (Stream a) where
+show x = Prelude.show (take 20 (streamToList x))
+
+--Ex04
+streamRepeat :: a -> Stream a
+streamRepeat x = SCons x (streamRepeat x)
+
+--streamMap :: (a -> b) -> Stream a -> Stream b
+--streamMap f (SCons x xs) = map f x
