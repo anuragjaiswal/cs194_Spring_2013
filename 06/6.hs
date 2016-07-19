39893c05c54ee244f68a07d8a906f7942fbaf245
ajaiswal <anuragjaiswal.07@gmail.com>
2016-07-19 16:53:08 +0530

    Ex05 of Homework 6

--- 06/6.hs
+++ 06/6.hs
@@ -30,5 +30,50 @@ show x = Prelude.show (take 20 (streamToList x))
 streamRepeat :: a -> Stream a
 streamRepeat x = SCons x (streamRepeat x)
 
---streamMap :: (a -> b) -> Stream a -> Stream b
---streamMap f (SCons x xs) = map f x
+--Ex06.show (streamRepeat 1)
+--"[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]"
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (SCons x xs) = SCons(f x) (streamMap f xs)
+
+-- Ex06.show (streamMap (+1) (streamRepeat 1))
+--"[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]"
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed r a = SCons a (streamFromSeed r (r a))
+
+--Ex06.show (streamFromSeed (+1) 1)
+--"[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]"
+--Ex06.show (streamFromSeed (+2) 1)
+--"[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]"
+--Ex06.show (streamFromSeed (+3) 3)
+--"[3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60]"
+
+--Ex05
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+--Ex06.show nats
+--"[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]"
+
+--ruler :: Stream Integer
+--ruler = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
